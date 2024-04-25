300094e6fdc86f7ea0e5dfe842c8a4d60559635b
Willy <thewillyeb@gmail.com>
2024-04-25 21:27:07 +0100

    FaqCard almost done, I think..

--- src/components/business/faqs/FaqCard.vue
+++ src/components/business/faqs/FaqCard.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
 
+import {PropType} from "vue";
+
+interface Faq {
+  question: string,
+  date: string,
+}
+
+const props = defineProps({
+    faq: {
+      type: Object as PropType<Faq>,
+      required: true,
+    }
+  })
 </script>
 
 <template>
@@ -7,15 +20,25 @@
     <section class="py-2 px-2">
         <p class="text-xs font-medium text-blue-500">Official answer</p>
     </section>
-    <section class="py-2 px-2 w-full">
-      <p class="text-1 text-center text-gray-500 font-medium">CUal es el trimeppo limtie .....</p>
+    <section class="py-4 px-2 w-full overflow-auto">
+      <p class="text-1 text-center text-gray-500 font-medium">
+        CUal es el trimeppo limtie .....
+        CUal es el trimeppo limtie .....
+        CUal es el trimeppo limtie .....
+        CUal es el trimeppo limtie .....
+        CUal es el trimeppo limtie .....
+        {{props.faq.question}}
+      </p>
     </section>
-    <section class="flex justify-between py-2 px-2 w-full">
-      <div class="bg-green-200 rounded-lg">
-        <p class="text-xs font-medium text-green-500">Administrativo</p>
+    <section class="flex justify-between items-start content-center py-2 px-2 w-full">
+      <div class="bg-green-200 rounded-lg py-1 px-1">
+        <p class="text-[0.6rem] font-medium text-green-500">Administrativo</p>
       </div>
-      <div class="flex flex-col">
-        asdasdasd
+      <div class="flex flex-col text-gray-500">
+        <div>
+          <p class="text-[0.6rem]">icon answers</p>
+        </div>
+        <p class="text-[0.5rem] font-normal">{{ props.faq.date }}</p>
       </div>
     </section>
   </article>
