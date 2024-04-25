3c38fa99f54e97cc45f064301d405b5323610f45
Willy <thewillyeb@gmail.com>
2024-04-25 01:23:54 +0100

    a

--- src/components/business/faqs/FaqCard.vue
+++ src/components/business/faqs/FaqCard.vue
@@ -3,7 +3,22 @@
 </script>
 
 <template>
-
+  <article class="faq-card flex flex-col w-full bg-gray-200 rounded-lg">
+    <section class="py-2 px-2">
+        <p class="text-xs font-medium text-blue-500">Official answer</p>
+    </section>
+    <section class="py-2 px-2 w-full">
+      <p class="text-1 text-center text-gray-500 font-medium">CUal es el trimeppo limtie .....</p>
+    </section>
+    <section class="flex justify-between py-2 px-2 w-full">
+      <div class="bg-green-200 rounded-lg">
+        <p class="text-xs font-medium text-green-500">Administrativo</p>
+      </div>
+      <div class="flex flex-col">
+        asdasdasd
+      </div>
+    </section>
+  </article>
 </template>
 
 <style scoped>
--- src/pages/base/pages/faqs/pages/faqsList/FaqsList.vue
+++ src/pages/base/pages/faqs/pages/faqsList/FaqsList.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import FaqCard from "../../../../../../components/business/faqs/FaqCard.vue";
 
 </script>
 
 <template>
   this is faqs list
+  <FaqCard></FaqCard>
 </template>
 
 <style scoped>
