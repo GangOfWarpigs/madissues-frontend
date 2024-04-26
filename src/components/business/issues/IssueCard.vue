1724e4333b2aa35e3b3c38b2790c7b5e3b1e9649
Sara González Ramírez <saragonza.lez0608@gmail.com>
2024-04-26 15:37:11 +0100

    issue card done without responsive or routing

--- src/components/business/issues/IssueCard.vue
+++ src/components/business/issues/IssueCard.vue
@@ -1,9 +1,42 @@
 <script setup lang="ts">
-
+    import { PropType } from 'vue';
+    import { Issue } from '../../../domain/issue';
+    
+    const props = defineProps({
+        issue: {
+            type: Object as PropType<Issue>,
+            required: true
+        }
+    });
 </script>
 
 <template>
-
+    <article class="flex w-full px-8 py-5 bg-gray-100 rounded-lg mt-5 text-slate-600 cursor-pointer hover:shadow-lg hover:shadow-gray-200">
+        <b-icon-record-circle class="text-green-500 text-2xl mr-6"/>
+        <div class="flex flex-col w-full">
+            <section class="flex items-center justify-between w-100">
+                <h1 class="font-semibold text-lg max-w-6xl whitespace-nowrap text-ellipsis overflow-hidden">{{ props.issue.question }}</h1>
+                <div class="flex text-slate-400 items-center text-center justify-center">
+                    <b-icon-chat-left-text-fill/>
+                    <p class="ml-2 mb-1">{{ props.issue.comments }}</p>
+                </div>
+            </section>
+            <section class="flex items-end justify-between w-100 mt-5">
+                <div class="flex items-center text-slate-500">
+                    <img class="w-12 border-2 border-white rounded-full" src="../../../assets/images/profile_picture_test.png" alt="Foto de perfil del usuario"/>
+                    <div class="flex flex-col mx-4 mb-2">
+                        <h2 class="font-semibold text-base">{{ props.issue.username }}</h2>
+                        <p class="text-xs">{{ props.issue.useryear }}</p>
+                    </div>
+                    <p class="text-xs">{{ props.issue.date }}</p>
+                </div>
+                <div class="flex items-center">
+                    <p class="text-sm font-semibold bg-green-100 text-green-500 px-2 py-1 rounded-md mr-4">{{ props.issue.subject }}</p>
+                    <p class="text-sm font-semibold bg-rose-100 text-rose-500 px-2 py-1 rounded-md">{{ props.issue.teacher }}</p>
+                </div>
+            </section>
+        </div>
+    </article>
 </template>
 
 <style scoped>
--- src/pages/base/pages/issues/pages/issuesList/IssuesList.vue
+++ src/pages/base/pages/issues/pages/issuesList/IssuesList.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
   import PageBanner from "../../../../../../components/shared/PageBanner.vue";
+  import IssueCard from "../../../../../../components/business/issues/IssueCard.vue";
+
+  const issueData = {
+    id: "2",
+    question: "El profesor no me deja realizar la revisión del examen de arquitectura de computadores",
+    state: "open",
+    comments: 7,
+    teacher: "Domingo",
+    subject: "Arquitectura de Computadores",
+    username: "Jose Peña Seco",
+    useryear: "3rd year",
+    date: "a minute ago"
+  }
 </script>
 
 <template>
@@ -7,6 +20,8 @@
   <div class="mx-16">
     <PageBanner title="Incidents" subtitle="Here you can report an incident" primaryColor="#1986E3"
                 secondaryColor="#978EFF" current-page="issues"></PageBanner>
+    <IssueCard :issue="issueData"/>
+    <IssueCard :issue="issueData"/>
   </div>
 </template>
 
