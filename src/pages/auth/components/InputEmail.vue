7adfcd39bb440de3bfed6daade2aaa775eaf5d75
Willy <thewillyeb@gmail.com>
2024-04-26 20:11:22 +0100

    InputEmail done

--- src/pages/auth/components/InputEmail.vue
+++ src/pages/auth/components/InputEmail.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import {useField} from 'vee-validate'
+import * as yup from 'yup'
 
 const props = defineProps({
   email: {
@@ -8,17 +9,22 @@ const props = defineProps({
   },
 });
 
-const {value, errorMessage } = useField(() => props.email);
+const emailRules = yup.string()
+    .required('Email is required')
+    .matches(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Invalid email address')
+    .email('Please enter a valid email address')
 
 
-
+const {value, errorMessage } = useField(props.email, emailRules);
 </script>
 
 <template>
-  <div class="flex space-x-2 rounded-b-lg bg-[#F5F5F5] w-full py-1 px-1 text-[#7C7C7C]">
-    <b-icon-envelope-fill class="h-5"/>
-    <input class="bg-[#F5F5F5] w-full text-[#7C7C7C] outline-none" v-model="value" placeholder="Email"/>
-    <span class="error" v-if="errorMessage">{{ errorMessage }}</span>
+  <div class="flex flex-col  rounded-2x w-full">
+    <div class="flex space-x-2 items-center bg-[#F5F5F5] w-full py-1 px-1 text-[#7C7C7C]">
+      <b-icon-envelope-fill class="h-5"/>
+      <input class="bg-[#F5F5F5] w-full text-[#7C7C7C] outline-none" v-model="value" placeholder="Email" :rules="emailRules"/>
+    </div>
+    <span class="text-red-500" v-if="errorMessage">{{ errorMessage }}</span>
   </div>
 
 </template>
--- src/pages/auth/pages/SignIn/SignIn.vue
+++ src/pages/auth/pages/SignIn/SignIn.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import InputEmail  from "../../components/InputEmail.vue";
 
-
 </script>
 
 <template>
