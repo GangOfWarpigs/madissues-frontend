4f126140bcdc8d34e51f1c3c66b6232de19fb783
DaniYDevs <danidzm18@gmail.com>
2024-04-27 20:32:58 +0100

    Change inputs info dialog

--- src/components/shared/InfoDialog.vue
+++ src/components/shared/InfoDialog.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
   interface Props{
     title: string,
-    description: string
+    content: string
   }
-  const {title, description} = withDefaults(defineProps<Props>(), {
+  const {title, content} = withDefaults(defineProps<Props>(), {
     title:"Aquí va el título de la página",
     description: "Breve descripción de la página"
   })
@@ -13,7 +13,7 @@
   <div class="w-full bg-sky-100 rounded-xl p-5 pl-8 border border-[#1986E3]">
     <b-icon-info-circle-fill class="mb-2 text-2xl text-[#1986E3]"/>
     <p class="text-2xl text-[#1986E3]">{{title}}</p>
-    <p class="text-[#1986E3]">{{description}}</p>
+    <p class="text-[#1986E3]">{{content}}</p>
   </div>
 </template>
 
