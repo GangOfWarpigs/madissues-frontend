06e98f4afc764bfcf61894bf34054f4dc9566a55
Willy <thewillyeb@gmail.com>
2024-04-27 14:45:05 +0100

    Form components partially done

--- src/pages/auth/components/InputEmail.vue
+++ src/pages/auth/components/InputEmail.vue
@@ -1,32 +1,23 @@
 <script setup lang="ts">
 import {useField} from 'vee-validate'
-import * as yup from 'yup'
 
-const props = defineProps({
-  email: {
-    type: String,
-    required: true,
-  },
-});
-
-const emailRules = yup.string()
-    .required('Email is required')
-    .matches(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Invalid email address')
-    .email('Please enter a valid email address')
+const props = defineProps<{
+  email: string,
+  placeholder: string,
+}>()
 
 
-const {value, errorMessage } = useField(props.email, emailRules);
+const { value, errorMessage } = useField<string>(() => props.email);
 </script>
 
 <template>
   <div class="flex flex-col  rounded-2x w-full">
     <div class="flex space-x-2 items-center bg-[#F5F5F5] w-full py-1 px-1 text-[#7C7C7C]">
       <b-icon-envelope-fill class="h-5"/>
-      <input class="bg-[#F5F5F5] w-full text-[#7C7C7C] outline-none" v-model="value" placeholder="Email" :rules="emailRules"/>
+      <input type="email" v-model="value" :placeholder="props.placeholder" class="bg-[#F5F5F5] w-full text-[#7C7C7C] outline-none" />
     </div>
     <span class="text-red-500" v-if="errorMessage">{{ errorMessage }}</span>
   </div>
-
 </template>
 
 <style scoped>
--- src/pages/auth/pages/SignIn/SignIn.vue
+++ src/pages/auth/pages/SignIn/SignIn.vue
@@ -1,17 +1,40 @@
 <script setup lang="ts">
 import InputEmail  from "../../components/InputEmail.vue";
+import InputPassword from "../../components/InputPassword.vue";
+import * as yup from 'yup';
+import {useForm} from "vee-validate";
+
+const schema = yup.object({
+  email: yup.string().required().email(),
+  password : yup
+      .string()
+      .required()
+      .matches(
+          /^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$/,
+          'Password must contain at least one uppercase letter, one number and must be at least 8 characters'
+      )
+});
+
+
+const { handleSubmit } = useForm<{email : string, password : string}>({
+  validationSchema: schema,
+  initialValues: {
+    email: "",
+    password: ""
+  }
+})
+
+const submit = handleSubmit((values) => {
+  console.log(values.email, values.password)
+});
 
 </script>
 
 <template>
-  <form>
-    <InputEmail email="willy@ejemplo.com"/>
-
+  <form @submit.prevent="submit">
+    <InputEmail email="email" placeholder="Email"/>
+    <InputPassword password="password" placeholder="Password"/>
     <button>Submit</button>
   </form>
 
 </template>
-
-<style scoped>
-
-</style>
