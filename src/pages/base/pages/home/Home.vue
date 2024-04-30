0906fcef9a761836ce7ebb2bdedfa74c6f20ed45
DaniYDevs <danidzm18@gmail.com>
2024-04-30 10:13:16 +0100

    Initial Subject Card

--- src/pages/base/pages/home/Home.vue
+++ src/pages/base/pages/home/Home.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-
+  import SubjectCard from "./components/SubjectCard.vue";
 </script>
 
 <template>
   this is the home
+  <SubjectCard/>
 </template>
 
 <style scoped>
---
+++ src/pages/base/pages/home/components/SubjectCard.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+  import {computed} from "vue";
+
+  interface Props{
+    title: string,
+    icon: string,
+    primaryColor: string,
+    secondaryColor: string,
+  }
+  const {title, icon, primaryColor, secondaryColor} = withDefaults(defineProps<Props>(), {
+    title:"Titulo de asignatura",
+    icon: "URL",
+    primaryColor:"#1986E3",
+    secondaryColor:"#978EFF",
+  })
+
+  const subject = {
+    title,
+    icon,
+    primaryColor,
+    secondaryColor
+  };
+
+  // Calculamos el estilo del degradado
+  const gradientStyle = computed(() => {
+    return {
+      backgroundImage: `linear-gradient(to right, ${subject.primaryColor}, ${subject.secondaryColor})`
+    };
+  });
+</script>
+
+<template>
+  <div :style="gradientStyle" class="w-full">
+    <p class="text-2xl">{{subject.title}}</p>
+    <p> {{subject.icon}} </p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
