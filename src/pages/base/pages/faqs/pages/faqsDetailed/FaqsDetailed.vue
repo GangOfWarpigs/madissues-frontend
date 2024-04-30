73ccf6b900b7b6febe308f89bf783d1a8eba983c
Sara González Ramírez <saragonza.lez0608@gmail.com>
2024-04-30 23:27:06 +0100

    empty faqs details component

--- src/pages/base/pages/faqs/pages/faqsDetailed/FaqsDetailed.vue
+++ src/pages/base/pages/faqs/pages/faqsDetailed/FaqsDetailed.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-
+  import FaqDetails from './components/FaqDetails.vue';
 </script>
 
 <template>
   this is faqs detailed
+  <FaqDetails/>
 </template>
 
 <style scoped>
