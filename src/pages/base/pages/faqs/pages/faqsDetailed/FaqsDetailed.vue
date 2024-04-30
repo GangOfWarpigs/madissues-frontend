3a0be27834bb37de8140b6335a22bb05196923f3
Sara González Ramírez <saragonza.lez0608@gmail.com>
2024-05-01 00:09:48 +0100

    faq details done

--- src/pages/base/pages/faqs/pages/faqsDetailed/FaqsDetailed.vue
+++ src/pages/base/pages/faqs/pages/faqsDetailed/FaqsDetailed.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
-  import FaqDetails from './components/FaqDetails.vue';
+	import FaqDetails from './components/FaqDetails.vue';
+
+	const faqData1 = {
+		id: "1",
+		date_time: "Today, 03:48 PM",
+		title: "¿Cuál es el tiempo límite para realizar el pago de la matrícula? ¿Si se me pasa la fecha límite pasa algo?",
+		content: "¿Cuál es el tiempo límite para realizar el pago de la matrícula? ¿Si se me pasa la fecha límite pasa algo? Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean maximus metus id justo molestie dictum.",
+		comments: ["2", "3"]
+	}
 </script>
 
 <template>
-  this is faqs detailed
-  <FaqDetails/>
+	this is faqs detailed
+	<div class="mx-[450px]">
+		<FaqDetails :faq="faqData1"/>
+	</div>
 </template>
 
 <style scoped>
