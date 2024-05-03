be77263dc5a8ead96f293df8a2c8b150a15da16f
DaniYDevs <danidzm18@gmail.com>
2024-05-03 01:13:44 +0100

    Changes in Subject Card

--- src/pages/base/pages/home/Home.vue
+++ src/pages/base/pages/home/Home.vue
@@ -3,8 +3,11 @@
 </script>
 
 <template>
-  this is the home
-  <SubjectCard/>
+
+  <p>this is the home</p>
+  <div class="w-full p-14">
+    <SubjectCard title="Fundamentos Fisicos de la Informatica" num-of-issues="4"/>
+  </div>
 </template>
 
 <style scoped>
--- src/pages/base/pages/home/components/SubjectCard.vue
+++ src/pages/base/pages/home/components/SubjectCard.vue
@@ -1,45 +1,37 @@
 <script setup lang="ts">
   import {computed} from "vue";
-
   interface Props{
-    title: string,
     icon: string,
+    title: string,
     primaryColor: string,
     secondaryColor: string,
+    numOfIssues: number
   }
-  const {title, icon, primaryColor, secondaryColor} = withDefaults(defineProps<Props>(), {
-    title:"Titulo de asignatura",
-    icon: "URL",
+  const {icon, title, primaryColor, secondaryColor, numOfIssues} = withDefaults(defineProps<Props>(), {
+    icon: "",
+    title:"Aquí va el título de la asignatura",
     primaryColor:"#1986E3",
     secondaryColor:"#978EFF",
+    numOfIssues: 0
   })
-
-  const subject = {
-    title,
-    icon,
-    primaryColor,
-    secondaryColor
-  };
-
   // Calculamos el estilo del degradado
   const gradientStyle = computed(() => {
     return {
-      backgroundImage: `linear-gradient(to right, ${subject.primaryColor}, ${subject.secondaryColor})`
+      backgroundImage: `linear-gradient(to right, ${primaryColor}, ${secondaryColor})`
     };
   });
 </script>
-
 <template>
-  <div :style="gradientStyle" class="w-full">
-    <p class="text-2xl">{{subject.title}}</p>
-    <p> {{subject.icon}} </p>
-    <v-stage ref="stage" :config="stageSize">
-      <v-layer>
-        <v-shape :config="shapeConfig"/>
-      </v-layer>
-    </v-stage>
+  <div :style="gradientStyle" class="flex flex-col justify-center relative w-1/4 rounded-2xl p-5 overflow-hidden h-[10em]">
+    <img src="../../../../../assets/images/subject_card_icon.svg" class="w-[2.4em] mb-1"/>
+    <p class="text-white text-xl mb-1">{{title}}</p>
+    <p class="text-white">{{numOfIssues}} issues</p>
+    <img src="../../../../../assets/images/subject_card_vectorA.svg" class="absolute -bottom-0 right-0 z-0 w-full"/>
+    <img src="../../../../../assets/images/subject_card_vectorB.svg" class="absolute -bottom-0 left-0 w-full" />
   </div>
 </template>
+
+
 
 <style scoped>
 
