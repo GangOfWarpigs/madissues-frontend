991087761d003212da775199d3c4d628b1311325
Willy <thewillyeb@gmail.com>
2024-05-05 17:52:32 +0100

    Finished component

--- src/pages/base/pages/profile/Profile.vue
+++ src/pages/base/pages/profile/Profile.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import ProfileNavBar from "../../../../components/shared/ProfileNavBar.vue";
+
 
 </script>
 
 <template>
   this is profile header
+  <ProfileNavBar></ProfileNavBar>
   <div>
     <RouterView></RouterView>
   </div>
