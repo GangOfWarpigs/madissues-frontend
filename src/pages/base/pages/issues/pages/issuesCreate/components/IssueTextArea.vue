c9c493ad3c36514ddf26a5f37dfe4e445f373404
Willy <thewillyeb@gmail.com>
2024-05-06 11:30:23 +0100

    Progresssssing

---
+++ src/pages/base/pages/issues/pages/issuesCreate/components/IssueTextArea.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  placeholder: string
+}>()
+
+</script>
+
+<template>
+  <textarea class="resize-none w-full h-32 rounded-[0.75rem] outline-0 py-2.5 px-2.5 font-normal" :placeholder="props.placeholder" ref="textarea"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/base/pages/issues/pages/issuesCreate/components/WriteIssueContent.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+import IssueInput from "../components/IssueInput.vue";
+import IssueTextArea from "../components/IssueTextArea.vue";
+</script>
+
+<template>
+  <section class="bg-[#F5F5F5] rounded-2xl flex flex-col w-full px-10 py-10 font-medium text-[#404040] text-sm">
+    <div class="w-full">
+      <p class="rounded-2xl">Title</p>
+      <IssueInput placeholder="e.g. It is possible to vote to change the date of an exam?"/>
+    </div>
+    <div>
+      <p class="rounded-2xl">Description</p>
+      <IssueTextArea placeholder="aosdi"></IssueTextArea>
+    </div>
+    <div>
+      <p class="rounded-2xl">Subject</p>
+      <IssueInput placeholder="e.g. It is possible to vote to change the date of an exam?"></IssueInput>
+    </div>
+    <div>
+      <p class="rounded-2xl">Teachers Implicated</p>
+
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
