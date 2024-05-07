5a11f7da955edf0e155ee42f1782d0aab737c5a0
Sara González Ramírez <saragonza.lez0608@gmail.com>
2024-05-07 20:44:05 +0100

    fixed pull request conflicts

--- src/components/shared/PageBanner.vue
+++ src/components/shared/PageBanner.vue
@@ -1,5 +1,8 @@
-  <script lang="ts" setup>
+<script lang="ts" setup>
   import {defineProps, defineEmits, computed, PropType } from "vue";
+    import IncidentChild from "./IncidentChild.vue";
+    import SearchBarChild from "./SearchBarChild.vue";
+    import ProfileChild from "../../pages/base/pages/profile/components/ProfileChild.vue";
 
     interface UserDataProps {
       first_name: string,
@@ -34,14 +37,20 @@
       };
     });
     const emits = defineEmits(['search']);
+
+    const handleSearch = (query: string) => {
+      emits('search', query);
+    };
   </script>
 
   <template>
     <div :style="gradientStyle" class="h-60  w-full mx-auto mt-6 pt-32 rounded-3xl flex flex-col justify-center items-center" >
       <p class="text-4xl font-bold mt-7 mb-8 text-white font-sans">{{ props.data?.title }}</p>
       <p class="text-xl text-white font-sans">{{ props.data.subtitle}}</p>
-      <div class="relative w-full">
-        <slot></slot>
+      <IncidentChild v-if="props.data?.currentPage === 'issues'" class="relative translate-y-14"/>
+      <SearchBarChild @search="handleSearch" v-if="props.data?.currentPage === 'faqs'" class="relative translate-y-14"/>
+      <div class="w-full p-16">
+        <ProfileChild v-if="props.data?.currentPage === 'profile'" class="relative translate-y-20" :user="props.user"/>
       </div>
       <!-- 
         <IncidentChild v-if="props.data?.currentPage === 'issues'" class="relative translate-y-14"/>
