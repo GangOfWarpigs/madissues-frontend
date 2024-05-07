6bd76ffdef00108710b387779c4aeacab1326bbf
Sara González Ramírez <saragonza.lez0608@gmail.com>
2024-05-07 21:04:34 +0100

    basic comment box component

---
+++ src/pages/base/pages/issues/pages/issuesDetailed/components/CommentBox.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="w-full flex flex-col px-10 py-5 bg-gray-100 rounded-lg box-border">
+        <p class="font-semibold text-sm text-slate-600">Write a comment</p>
+        <textarea class="w-full rounded-lg bg-white px-3 py-2 min-h-28 my-2 box-border max-w-full"></textarea>
+        <div class="w-full flex justify-end items-center">
+            <button class="text-xs font-semibold text-white px-3 py-1 rounded bg-sky-500">Publish</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
