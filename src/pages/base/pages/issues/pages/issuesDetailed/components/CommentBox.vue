34b51fd219ab2be099145a5b8a0127acf4f3afc4
Sara González Ramírez <saragonza.lez0608@gmail.com>
2024-05-07 21:46:02 +0100

    added functionality

--- src/pages/base/pages/issues/pages/issuesDetailed/components/CommentBox.vue
+++ src/pages/base/pages/issues/pages/issuesDetailed/components/CommentBox.vue
@@ -1,13 +1,36 @@
 <script setup lang="ts">
+    import { ref } from 'vue';
 
+    const comment = ref('');
+    const maxCharacters = 400;
+    const isLimit = ref(false);
+
+    const emit = defineEmits(['newComment']);
+
+    function handleInput(event: InputEvent) {
+        const target = event.target as HTMLTextAreaElement;
+        comment.value = target.value.slice(0, maxCharacters);
+        isLimit.value = comment.value.length >= maxCharacters;
+    }
+
+    function publishComment() {
+        emit('newComment', comment.value);
+    }
 </script>
 
 <template>
-    <div class="w-full flex flex-col px-10 py-5 bg-gray-100 rounded-lg box-border">
+    <div class="w-full flex flex-col px-8 py-5 bg-gray-100 rounded-lg box-border">
         <p class="font-semibold text-sm text-slate-600">Write a comment</p>
-        <textarea class="w-full rounded-lg bg-white px-3 py-2 min-h-28 my-2 box-border max-w-full"></textarea>
-        <div class="w-full flex justify-end items-center">
-            <button class="text-xs font-semibold text-white px-3 py-1 rounded bg-sky-500">Publish</button>
+        <textarea 
+            class="w-full rounded-lg bg-white px-3 py-2 min-h-28 my-3 box-border max-w-full"
+            v-model="comment"
+            @input="handleInput"    
+        ></textarea>
+        <div class="w-full flex justify-between items-start">
+            <div>
+                <p class="text-xs text-red-600" v-if="isLimit">Reached max characters</p>
+            </div>
+            <button @click="publishComment" class="self text-sm font-semibold text-white px-3 py-1 rounded bg-sky-500 cursor-pointer active:scale-95">Publish</button>
         </div>
     </div>
 </template>
