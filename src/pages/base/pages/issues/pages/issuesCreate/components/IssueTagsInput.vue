9f83638cf0a00ae930a49ce1a06a6f0db09a9b8e
Willy <thewillyeb@gmail.com>
2024-05-07 10:03:34 +0100

    Finished creo

--- src/pages/base/pages/issues/pages/issuesCreate/components/IssueTagsInput.vue
+++ src/pages/base/pages/issues/pages/issuesCreate/components/IssueTagsInput.vue
@@ -1,28 +1,44 @@
 <script setup lang="ts">
-import { TagsInputInput, TagsInputItem, TagsInputItemDelete, TagsInputItemText, TagsInputRoot } from 'radix-vue'
-import {ref} from "vue";
+import { TagsInputItem, TagsInputItemDelete, TagsInputItemText, TagsInputRoot } from 'radix-vue'
+import {Ref, ref} from "vue";
 
 import { Icon } from '@iconify/vue'
 
-const modelValue = ref([])
+const modelValue:Ref<string[]> = ref([])
 const allowedTags = ['Marilola', 'Domingo', 'Carmelo']
+const selectedTag = ref('')
 
+const addTag = () => {
+  if (selectedTag.value && !modelValue.value.includes(selectedTag.value)) {
+    modelValue.value.push(selectedTag.value)
+  }
+}
+
+const deleteTag = () => {
+  selectedTag.value = ''
+}
 
 </script>
 
 <template>
-  <TagsInputRoot
-      v-model="modelValue"
-      class="flex gap-2 items-center p-2 rounded-[0.75rem] bg-white w-full flex-wrap"
-  >
-    <TagsInputItem v-for="item in modelValue" :key="item" :value="item" class="flex items-center justify-center gap-2 bg-green-200 rounded p-1">
-      <TagsInputItemText class="text-sm pl-1" />
-      <TagsInputItemDelete class="p-0.5 rounded">
-        <Icon icon="lucide:x" />
-      </TagsInputItemDelete>
-    </TagsInputItem>
-    <TagsInputInput placeholder="Write..." class="text-sm focus:outline-none flex-1 rounded px-1" :options="allowedTags "/>
-  </TagsInputRoot>
+  <div class="flex flex-col w-full ">
+    <TagsInputRoot
+        v-model="modelValue"
+        class="flex gap-2 items-center w-full flex-wrap"
+    >
+      <TagsInputItem v-for="item in modelValue" :key="item" :value="item" class="flex items-center justify-center gap-2 bg-green-200 my-2 rounded p-1 text-[#404040]">
+        <TagsInputItemText class="" />
+        <TagsInputItemDelete class="p-0.5 rounded" @click="deleteTag">
+          <Icon icon="lucide:x" />
+        </TagsInputItemDelete>
+      </TagsInputItem>
+    </TagsInputRoot>
+    <select v-model="selectedTag" @change="addTag" class="focus:outline-none flex-1 px-2.5 py-2.5 bg-white rounded-[0.75rem] font-normal ">
+      <option disabled value="">Select a teacher</option>
+      <option v-for="tag in allowedTags" :key="tag" :value="tag">{{ tag }}</option>
+    </select>
+  </div>
+
 </template>
 
 <style scoped>
--- src/pages/base/pages/issues/pages/issuesCreate/components/WriteIssueContent.vue
+++ src/pages/base/pages/issues/pages/issuesCreate/components/WriteIssueContent.vue
@@ -6,20 +6,20 @@ import IssueTagSelector from "../components/IssueTagsInput.vue";
 </script>
 
 <template>
-  <section class="bg-[#F5F5F5] rounded-2xl flex flex-col w-full px-10 py-10 font-medium text-[#404040] text-sm">
-    <div class="w-full">
+  <section class="bg-[#F5F5F5] rounded-2xl flex flex-col w-full px-10 py-10 font-medium text-sm gap-4">
+    <div class="w-full flex flex-col gap-1">
       <p class="rounded-2xl">Title</p>
       <IssueInput placeholder="e.g. It is possible to vote to change the date of an exam?"/>
     </div>
-    <div>
+    <div class="w-full flex flex-col gap-1">
       <p class="rounded-2xl">Description</p>
-      <IssueTextArea placeholder="aosdi"></IssueTextArea>
+      <IssueTextArea placeholder="e.g. It is possible to vote to change the date of an exam?"></IssueTextArea>
     </div>
-    <div>
+    <div class="w-full flex flex-col gap-1">
       <p class="rounded-2xl">Subject</p>
       <IssueInput placeholder="e.g. It is possible to vote to change the date of an exam?"></IssueInput>
     </div>
-    <div>
+    <div class="w-full flex flex-col gap-1">
       <p class="rounded-2xl">Teachers Implicated</p>
       <IssueTagSelector></IssueTagSelector>
     </div>
