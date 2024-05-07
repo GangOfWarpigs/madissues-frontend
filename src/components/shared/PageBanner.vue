306e5b13dc24fc9458d849cf824bd3cfc58b6887
Sara González Ramírez <saragonza.lez0608@gmail.com>
2024-05-07 20:52:58 +0100

    fixed pull requests conflicts

--- src/components/shared/PageBanner.vue
+++ src/components/shared/PageBanner.vue
@@ -1,24 +1,25 @@
 <script lang="ts" setup>
   import {defineProps, defineEmits, computed, PropType } from "vue";
-    import IncidentChild from "./IncidentChild.vue";
-    import SearchBarChild from "./SearchBarChild.vue";
-    import ProfileChild from "../../pages/base/pages/profile/components/ProfileChild.vue";
+  import IncidentChild from "./IncidentChild.vue";
+  import SearchBarChild from "./SearchBarChild.vue";
+  import ProfileChild from "../../pages/base/pages/profile/components/ProfileChild.vue";
 
-    interface UserDataProps {
-      first_name: string,
-      last_name: string,
-      degree: string,
-      last_school_year: string,
-      email: string
-    }
+  interface UserDataProps {
+    first_name: string,
+    last_name: string,
+    degree: string,
+    last_school_year: string,
+    email: string
+  }
 
-    interface Props{
-      title: string,
-      subtitle: string,
-      primaryColor: string,
-      secondaryColor: string,
-      currentPage: string,
-    }
+  interface Props{
+    title: string,
+    subtitle: string,
+    primaryColor: string,
+    secondaryColor: string,
+    currentPage: string,
+  }
+  
   const props = defineProps({
     data: {
       type: Object as PropType<Props>,
@@ -30,37 +31,30 @@
     }
   });
 
-  // Calculamos el estilo del degradado
-    const gradientStyle = computed(() => {
-      return {
-        backgroundImage: `linear-gradient(to right, ${props.data?.primaryColor}, ${props.data?.secondaryColor})`
-      };
-    });
-    const emits = defineEmits(['search']);
-
-    const handleSearch = (query: string) => {
-      emits('search', query);
+  const gradientStyle = computed(() => {
+    return {
+      backgroundImage: `linear-gradient(to right, ${props.data?.primaryColor}, ${props.data?.secondaryColor})`
     };
-  </script>
+  });
+  
+  const emits = defineEmits(['search']);
 
-  <template>
-    <div :style="gradientStyle" class="h-60  w-full mx-auto mt-6 pt-32 rounded-3xl flex flex-col justify-center items-center" >
-      <p class="text-4xl font-bold mt-7 mb-8 text-white font-sans">{{ props.data?.title }}</p>
-      <p class="text-xl text-white font-sans">{{ props.data.subtitle}}</p>
-      <IncidentChild v-if="props.data?.currentPage === 'issues'" class="relative translate-y-14"/>
-      <SearchBarChild @search="handleSearch" v-if="props.data?.currentPage === 'faqs'" class="relative translate-y-14"/>
-      <div class="w-full p-16">
-        <ProfileChild v-if="props.data?.currentPage === 'profile'" class="relative translate-y-20" :user="props.user"/>
-      </div>
-      <!-- 
-        <IncidentChild v-if="props.data?.currentPage === 'issues'" class="relative translate-y-14"/>
-        <SearchBarChild @search="handleSearch" v-if="props.data?.currentPage === 'faqs'" class="relative translate-y-14"/>
-        <div class="w-full">
-          <ProfileChild v-if="props.data?.currentPage === 'profile'" class="relative translate-y-20" :user="props.user"/>
-        </div>
-      -->
+  const handleSearch = (query: string) => {
+    emits('search', query);
+  };
+</script>
+
+<template>
+  <div :style="gradientStyle" class="h-60 w-full mx-auto mt-6 pt-32 mb-5 rounded-3xl flex flex-col justify-center items-center" >
+    <p class="text-4xl font-bold mt-7 mb-8 text-white font-sans">{{ props.data?.title }}</p>
+    <p class="text-xl text-white font-sans">{{ props.data.subtitle}}</p>
+    <IncidentChild v-if="props.data?.currentPage === 'issues'" class="relative translate-y-14"/>
+    <SearchBarChild @search="handleSearch" v-if="props.data?.currentPage === 'faqs'" class="relative translate-y-14"/>
+    <div class="w-full px-10 pb-36">
+      <ProfileChild v-if="props.data?.currentPage === 'profile'" class="relative translate-y-20" :user="props.user"/>
     </div>
-  </template>
+  </div>
+</template>
 
 <style scoped>
 
--- src/pages/base/pages/profile/components/ProfileChild.vue
+++ src/pages/base/pages/profile/components/ProfileChild.vue
@@ -20,8 +20,8 @@
     <div class="rounded-[50%] border-8 border-white w-[11em]">
       <img src="../../../../../assets/images/profile_picture_test.png"/>
     </div>
-    <div>
-      <p class="text-3xl font-semibold">{{props.user.first_name}} {{props.user.last_name}}</p>
+    <div class="ml-3">
+      <p class="text-2xl font-semibold">{{props.user.first_name}} {{props.user.last_name}}</p>
       <p class="text-xl text-gray-500">{{props.user.degree}}, {{props.user.last_school_year}}º curso</p>
     </div>
   </div>
