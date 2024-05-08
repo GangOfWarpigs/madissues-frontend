638ee9eb47184a1a3232a0e8601b42e1ffffa1bd
Willy <thewillyeb@gmail.com>
2024-05-08 11:43:51 +0100

    Added post and discard buttons

--- src/pages/base/pages/issues/pages/issuesCreate/IssuesCreate.vue
+++ src/pages/base/pages/issues/pages/issuesCreate/IssuesCreate.vue
@@ -8,6 +8,14 @@ import IssueProofs from "../issuesCreate/components/IssueProofs.vue";
   <main class="flex flex-col gap-10 ">
     <WriteIssueContent></WriteIssueContent>
     <IssueProofs></IssueProofs>
+    <div class="flex justify-between text-white font-medium text-sm">
+      <button class="rounded-lg bg-[#FF7D7D] py-1 px-5">
+        Discard
+      </button>
+      <button class="rounded-lg bg-[#1986E3] py-1 px-5">
+        Post the issue
+      </button>
+    </div>
   </main>
 
 </template>
