304fca601edc9938f7956eac1fafa459a4af7495
Sara González Ramírez <saragonza.lez0608@gmail.com>
2024-05-12 01:32:57 +0100

    fixed asynx errors

--- src/pages/base/pages/issues/pages/issuesList/IssuesList.vue
+++ src/pages/base/pages/issues/pages/issuesList/IssuesList.vue
@@ -2,9 +2,10 @@
     import PageBanner from "../../../../../../components/shared/pageBanner/PageBanner.vue";
     import ButtonChild from "../../../../../../components/shared/pageBanner/childs/ButtonChild.vue";
     import IssueDisplay from "../../../../../../components/business/issues/IssueDisplay.vue";
-    import {useRoute} from "vue-router";
-    import {useQuery} from "@tanstack/vue-query";
-    import {getOrganizationIssues, Issue} from "../../../../../../api/organizations.ts";
+    import { useRoute } from "vue-router";
+    import { useQuery } from "@tanstack/vue-query";
+    import { getOrganizationIssues, Issue } from "../../../../../../api/organizations.ts";
+    import { ref, watch } from "vue";
 
     const pageProps = {
         title: "Incidents",
@@ -15,11 +16,19 @@
 
     const route = useRoute();
     const organizationId = route.params["organization_id"] as string;
+    const issues = ref();
 
     const { data, isSuccess } = useQuery<Issue[]>({
       queryKey: ["organization", organizationId],
       queryFn: async () => await getOrganizationIssues(organizationId),
     });
+
+    watch(data, () => {
+        if (data !== undefined) {
+            issues.value = data.value;
+            console.log(issues.value);
+        }
+    })
 </script>
 
 <template>
@@ -27,6 +36,6 @@
         <ButtonChild route="./issues/create" text="I have an incident"/>
     </PageBanner>
     <div class="w-full mt-10">
-        <IssueDisplay :issues="data!" v-if="isSuccess"/>
+        <IssueDisplay :issues="issues" v-if="isSuccess && data !== undefined"/>
     </div>
 </template>
