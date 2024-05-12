efb248612ff371d36d35d927404592fc54b37e11
Sara González Ramírez <saragonza.lez0608@gmail.com>
2024-05-12 19:19:07 +0100

    removed unnecessary imports

--- src/pages/base/pages/issues/pages/issuesDetailed/components/IssueDetails.vue
+++ src/pages/base/pages/issues/pages/issuesDetailed/components/IssueDetails.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-    import { PropType, ref, computed } from 'vue';
+    import { PropType, computed } from 'vue';
     import { Issue } from '../../../../../../../api/organizations';
     import { baseURL } from '../../../../../../../api/client';
 
